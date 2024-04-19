f22f9ee25bc473e07a1b9d2638b78743c8802485
Daniel Heid <mail@daniel-heid.de>
2024-04-19 22:40:04 +0200

    Differentiate OS in JVMTest #56

--- src/test/java/com/bric/colorpicker/JVMTest.java
+++ src/test/java/com/bric/colorpicker/JVMTest.java
@@ -3,6 +3,8 @@ package com.bric.colorpicker;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class JVMTest {
 
@@ -15,6 +17,17 @@ class JVMTest {
 
     }
 
+    @EnabledOnOs(value = {OS.LINUX, OS.WINDOWS, OS.AIX, OS.FREEBSD, OS.OTHER, OS.OPENBSD, OS.SOLARIS})
+    @Test
+    void checksWhetherSystemIsNotMac() {
+
+        boolean isMac = JVM.IS_MAC;
+
+        assertThat(isMac).isFalse();
+
+    }
+
+    @EnabledOnOs(value = OS.MAC)
     @Test
     void checksWhetherSystemIsMac() {
 
@@ -22,6 +35,6 @@ class JVMTest {
 
         assertThat(isMac).isFalse();
 
-
     }
+
 }
